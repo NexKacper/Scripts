54cf4a2215b947dead96e6ac1be8f2a841545607
NexKacper <120577129+NexKacper@users.noreply.github.com>
2023-01-17 12:11:45 +0100

    Update okhwhoaskeadad.lua

--- okhwhoaskeadad.lua
+++ okhwhoaskeadad.lua
@@ -1,1 +1,17 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/minecrafttotem/yzhs./main/Fe%20pp%20script%20very%20fun"))()
+wait(1)
+loadstring(game:HttpGet(('https://pastebin.com/raw/Cu7bKQWN'),true))()
+wait(1)
+for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
+    if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
+    game:GetService("RunService").Heartbeat:connect(function()
+    v.Velocity = Vector3.new(-30,0,0)
+    end)
+    end
+    end
+ 
+    game:GetService("StarterGui"):SetCore("SendNotification", { 
+        Title = "Notification";
+        Text = "Netless Ran";
+        Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+    Duration = 16;
